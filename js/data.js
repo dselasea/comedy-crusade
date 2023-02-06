e91c6d9a0af365cca74b0919bfc72d6dfc8361df
Daniel <dselasea@gmail.com>
2023-02-06 14:39:54 +0000

    feat: Add program section dynamically and project styles

--- js/data.js
+++ js/data.js
@@ -1,8 +1,3 @@
-const menu = document.querySelector('.menu__logo--hamburger');
-const menuLink = document.querySelector('.menu__link');
-
-let programContent = document.querySelector('.program__content');
-
 const program = [
   {
     img: '',
@@ -31,21 +26,3 @@ const program = [
   },
 
 ]
-
-menu.addEventListener('click', openMenu);
-
-let htmlContent = '';
-
-for(let i = 0; i < program.length; i += 1){
-  htmlContent += `
-  <div class="programs">
-  <h4>${program[i].title}</h4>
-  <p>${program[i].description}</p>
-  </div>`;
-  programContent.innerHTML = htmlContent;
-}
-
-function openMenu() {
-  menuLink.classList.toggle('open');
-  console.log('clicked')
-}

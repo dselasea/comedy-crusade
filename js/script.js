2b7fee68f364c52d380b74d3d36c0e1b94a7f1fd
Daniel <dselasea@gmail.com>
2023-02-06 13:24:58 +0000

    feat: Add menu and menu function, add content for header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('Working');
+const menu = document.querySelector('.menu__logo--hamburger');
+const menuLink = document.querySelector('.menu__link');
+
+menu.addEventListener('click', openMenu);
+
+function openMenu() {
+  menuLink.classList.toggle('open');
+  console.log('clicked')
+}

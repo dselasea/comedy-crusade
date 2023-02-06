3d996bd778fe33de40946cd2c8d71000b75e53d9
Daniel <dselasea@gmail.com>
2023-02-06 22:24:08 +0000

    feat: Add content for speakers and styles

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const menu = document.querySelector('.menu__logo--hamburger');
 const menuLink = document.querySelector('.menu__link');
 
 let programContent = document.querySelector('.program__content');
+let speakerContent = document.querySelector('.speaker__content');
+
+menu.addEventListener('click', openMenu);
 
 const program = [
   {
@@ -34,50 +37,78 @@ const program = [
 
 const speakers = [
   {
-    img: '<i class="fa-solid fa-blog fa-2x"></i>',
-    speaker: 'Blogging',
-    title: 'Professor',
+    img: './images/speaker-1.jpg',
+    chessImg: './images/speaker-back.png',
+    speaker: 'OB Amponsah',
+    title: 'Stand-up Comedian/Optometrist',
     description: 'Listen to the speakers from various countries about the messages of sharing and opening.'
   },
   {
-    img: '<i class="fa-solid fa-chess-rook fa-2x"></i>',
-    speaker: 'Strategy',
-    title: 'Teacher',
+    img: './images/speaker-2.jpg',
+    chessImg: './images/speaker-back.png',
+    speaker: 'Khemikal',
+    title: 'Stand-up Comedian/Software Engineer',
     description: 'Listen to the speakers from various countries about the messages of sharing and opening.'
   },
   {
-    img: '<i class="fa-solid fa-cart-shopping fa-2x"></i>',
-    speaker: 'Marketing',
-    title: 'Student',
+    img: './images/speaker-3.jpg',
+    chessImg: './images/speaker-back.png',
+    speaker: 'Lekzy De Comic',
+    title: 'Stand-up Comedian/Interior Decorator',
     description: 'Listen to the speakers from various countries about the messages of sharing and opening.'
   },
   {
-    img: '<i class="fa-brands fa-adversal fa-2x"></i>',
-    speaker: 'Adversing',
-    title: 'Marketer',
+    img: './images/speaker-4.jpg',
+    chessImg: './images/speaker-back.png',
+    speaker: 'DKB',
+    title: 'Stand-up Comedian/Actor',
     description: 'Listen to the speakers from various countries about the messages of sharing and opening.'
   },
   {
-    img: '<i class="fa-solid fa-globe fa-2x"></i>',
-    speaker: 'Websites',
-    title: 'Sales Person',
+    img: './images/speaker-5.jpg',
+    chessImg: './images/speaker-back.png',
+    speaker: 'Putogo',
+    title: 'Stand-up comedian/Content Creator',
+    description: 'Listen to the speakers from various countries about the messages of sharing and opening.'
+  },
+  {
+    img: './images/speaker-6.jpg',
+    chessImg: './images/speaker-back.png',
+    speaker: 'MJ the Comedian',
+    title: 'Stand-up Comedian/Content Creator',
     description: 'Listen to the speakers from various countries about the messages of sharing and opening.'
   },
 
-]
-
-menu.addEventListener('click', openMenu);
+];
 
 let htmlContent = '';
+let htmlSpeakerContent = '';
 
 for(let i = 0; i < program.length; i += 1){
   htmlContent += `
   <div class="programs">
-  <span>${program[i].icon}</span>
+  ${program[i].icon}
   <h4 class='programs__title'>${program[i].title}</h4>
   <p class='programs__description'>${program[i].description}</p>
   </div>`;
   programContent.innerHTML = htmlContent;
+}
+
+// Speakers
+for(let i = 0; i < speakers.length; i += 1){
+  htmlSpeakerContent += `
+  <div class="speaker__container">
+      <div class="speaker__content--img">
+      <img src='${speakers[i].chessImg}' alt="Chess Background" class="speaker__chess">
+      <img src='${speakers[i].img}' alt="Speaker Images" class="speaker__person">
+      </div>
+      <div class="speaker__content--main">
+        <h2 class="speaker__content--name">${speakers[i].title}</h2>
+        <p class="speaker__content--title">Berkman</p>
+        <p class="speaker__content--description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta error quidem blanditiis ex provident repellat vitae vel ipsum vero debitis?</p>
+      </div>
+  </div>`;
+  speakerContent.innerHTML = htmlSpeakerContent;
 }
 
 function openMenu() {
